9a1cb05a2fef8ace362d4f7e0fdfa9c3fee1c5e3
LakshmiNarayanareddy8 <annareddylakshminarayanareddy@gmail.com>
2026-03-02 08:15:08 +0530

    Backend ready for vercel

--- Backend/server.js
+++ Backend/server.js
@@ -1,41 +1,40 @@
-  require("dotenv").config();
-  const express = require("express");
-  const cors = require("cors");
-  const path = require("path");
-  const connectDB = require("./config/db");
-  const authRoutes = require("./routes/authRoutes");
-  const incomeRoutes = require("./routes/incomeRoutes");
-  const expenseRoutes = require("./routes/expenseRoutes");
-  const dashboardRoutes = require("./routes/dashboardRoutes");
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const path = require("path");
+const connectDB = require("./config/db");
+const authRoutes = require("./routes/authRoutes");
+const incomeRoutes = require("./routes/incomeRoutes");
+const expenseRoutes = require("./routes/expenseRoutes");
+const dashboardRoutes = require("./routes/dashboardRoutes");
 
-  const app = express();
+const app = express();
 
-  // Middleware to handle CORS
-  app.use(
-    cors({
-      // origin: process.env.CLIENT_URL || "http://localhost:5173",
-      origin: "*",
-      methods: ["GET", "POST", "PUT", "DELETE"],
-      allowedHeaders: ["Content-Type", "Authorization"],
-    })
-  );
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"]
+  })
+);
 
-  // Middleware to parse JSON
-  app.use(express.json());
+app.use(express.json());
 
-  connectDB();
+connectDB();
 
-  app.use("/api/v1/auth", authRoutes);
-  app.use("/api/v1/income", incomeRoutes);
-  app.use("/api/v1/expense",expenseRoutes);
-  app.use("/api/v1/dashboard",dashboardRoutes);
+app.use("/api/v1/auth", authRoutes);
+app.use("/api/v1/income", incomeRoutes);
+app.use("/api/v1/expense", expenseRoutes);
+app.use("/api/v1/dashboard", dashboardRoutes);
 
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
-  app.use("/uploads", express.static(path.join(__dirname, "uploads")));
-  // Port configuration
-  const PORT = process.env.PORT || 8000;
+app.get("/", (req, res) => {
+  res.send("Backend API Running");
+});
 
-  // Start server
-  app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-  }); 
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log("Server running on port " + PORT);
+});
